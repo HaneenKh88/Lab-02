8919da35e72321fa2510647b3f926e6723e98357
haneen <haneen.khasawneh96@gmail.com>
2021-01-05 22:06:31 +0200

    first

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
         
          var username = prompt('please enter your name');
          console.log ('please enter your name',username);
+         var Mark = 0;
          
          
         
@@ -10,11 +11,13 @@
         {
             alert('Oops! Your Aswer is Wrong.');
             console.log('Oops! Your Aswer is Wrong.');
+
         }
         else if (Qustion1.toLowerCase() === 'no' || Qustion1.toLowerCase() === 'n')
         {
             alert('Wow, Your Answer is Correct. Im graduated from Jordan University of Science and Technology.');
             console.log('Wow, Your Answer is Correct. Im graduated from Jordan University of Science and Technology.');
+            Mark ++;
         }
 
 
@@ -23,6 +26,7 @@
         {
             alert('Wow, Your Answer is Correct.');
             console.log('Wow, Your Answer is Correct.');
+            Mark ++;
         }
         else if (Qustion2.toLowerCase() === 'no' || Qustion2.toLowerCase() === 'n')
         {
@@ -40,6 +44,7 @@
         {
             alert('Wow, Your Answer is Correct. Im 24 years old.');
             console.log('Wow, Your Answer is Correct. Im 24 years old.')
+            Mark++;
         }
 
 
@@ -53,6 +58,7 @@
         {
             alert('Wow, Your Answer is Correct. my journey in the coding world starts with HTU.');
             console.log('Wow, Your Answer is Correct. my journey in the coding world starts with HTU.')
+            Mark++;
         }
 
 
@@ -60,13 +66,86 @@
         if (Qustion5.toLowerCase() === 'yes' || Qustion5.toLowerCase() === 'y' )
         {
             alert('Wow, Your Answer is Correct.');
-            console.log('Wow, Your Answer is Correct.')
+            console.log('Wow, Your Answer is Correct.');
+            Mark++;
         }
         else if (Qustion5.toLowerCase() === 'no' || Qustion5.toLowerCase() === 'n')
         {
             alert('Oops! Your Aswer is Wrong.');
             console.log('Oops! Your Aswer is Wrong.')
         }
+
+        
+        
+        // Lab 03 Code 
+
+        var TimesNumber = 4;
+       
+    
+            for(var i = 0 ; i < 4 ; i++)
+            {
+                var Qustion6 = prompt('How Many Times I Visit Dead Sea?');
+
+                 if (parseInt(Qustion6) > TimesNumber)
+                {
+                    alert('Too High');
+                    continue;
+                    
+                }
+
+                else if (parseInt(Qustion6) < 4)
+                {
+                    alert('Too Low');
+                    continue;
+                    
+                }
+
+                else if (parseInt(Qustion6) === 4)
+                {
+                    alert("Your Answer is correct");
+                    Mark++;
+                    break;
+                }
+                
+            }
+            
+            if(parseInt(Qustion6) !== 4)
+            {
+                alert("Your Answer is wrong, I visit Dead Sea"  + TimesNumber + " Times");
+            }
+
+
+           var color = ['red' , 'black' , 'grey' , 'brown'];
+           
+
+           for(var i = 0 ; i < 6 ; i++)
+           {
+            
+            var Qustion7 = prompt('Name one of my Faverote colors');
+            
+               if(Qustion7.toLowerCase() === 'red' || Qustion7.toLowerCase() === 'black' || Qustion7.toLowerCase() === 'grey' || Qustion7.toLowerCase() === 'brown')
+               {
+                   Mark ++;
+                   break;
+               }
+
+               else 
+               {
+                   alert('Please try again');
+                   continue;
+               }
+
+           }
+        
+
+        alert('My Favarote Colors is:  ' + color  + ' ');
+        alert ('Your total Score is : ' + Mark + ' out of 7. ');
+
+        
+        
+
+      
+
         
         alert('Wlcome ' + username + ' in my website.');
         document.write('<p> </p>');
